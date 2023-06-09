b105411e46a03f8a9d0f5a50effd965248b5d7ef
032AbhishekChoksi <19bmiit032@gmail.com>
2023-06-09 15:33:36 +0530

    Create Bottom Tab and Display all Users

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,12 +1,36 @@
+/* eslint-disable react-native/no-inline-styles */
 //import liraries
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import Users from '../tabs/Users';
+import Setting from '../tabs/Setting';
 
 // create a component
 const Main = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Main</Text>
+      {selectedTab === 0 ? <Users /> : selectedTab === 1 ? <Setting /> : null}
+      <View style={styles.bottomTab}>
+        <TouchableOpacity style={styles.tab} onPress={() => setSelectedTab(0)}>
+          <Image
+            source={require('../images/users.png')}
+            style={[
+              styles.tabIcon,
+              {tintColor: selectedTab === 0 ? '#ffffff' : '#704c00'},
+            ]}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.tab} onPress={() => setSelectedTab(1)}>
+          <Image
+            source={require('../images/setting.png')}
+            style={[
+              styles.tabIcon,
+              {tintColor: selectedTab === 1 ? '#ffffff' : '#704c00'},
+            ]}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -15,9 +39,25 @@ const Main = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
+  },
+  bottomTab: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: 70,
+    backgroundColor: '#ffa611',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  tab: {
+    width: '50%',
+    height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
   },
+  tabIcon: {width: 30, height: 30},
 });
 
 //make this component available to the app
---
+++ src/tabs/Users.js
@@ -0,0 +1,105 @@
+//import liraries
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+  Image,
+} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Loader from '../components/Loader';
+
+// create a component
+const Users = () => {
+  const [visible, setVisible] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    setVisible(true);
+    let tempData = [];
+    const email = await AsyncStorage.getItem('EMAIL');
+
+    firestore()
+      .collection('users')
+      .where('email', '!=', email)
+      .get()
+      .then(res => {
+        setVisible(false);
+        if (res.docs !== []) {
+          res.forEach(element => tempData.push(element.data()));
+        }
+        setUsers(tempData);
+      })
+      .catch(error => {
+        setVisible(false);
+        console.log(error);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>RN Firebase Chat App</Text>
+      </View>
+      <FlatList
+        data={users}
+        renderItem={({item, index}) => {
+          return (
+            <View style={styles.userItem}>
+              <Image
+                source={require('../images/user.png')}
+                style={styles.userIcon}
+              />
+              <Text style={styles.userName}>{item.name}</Text>
+            </View>
+          );
+        }}
+      />
+      <Loader visible={visible} />
+    </View>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  header: {
+    width: '100%',
+    height: 60,
+    backgroundColor: 'white',
+    elevation: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {color: '#ffa611', fontSize: 20, fontWeight: '600'},
+  userItem: {
+    width: Dimensions.get('window').width - 50,
+    alignSelf: 'center',
+    marginTop: 20,
+    flexDirection: 'row',
+    height: 60,
+    borderWidth: 0.5,
+    borderRadius: 10,
+    paddingLeft: 20,
+    alignItems: 'center',
+  },
+  userIcon: {width: 40, height: 40},
+  userName: {
+    color: 'black',
+    marginLeft: 20,
+    fontSize: 20,
+  },
+});
+
+//make this component available to the app
+export default Users;
